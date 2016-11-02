4de20f0deadcda6b53aabe57643073e72f28aa5e
Carl Eiserman <carleiserman@gmail.com>
2016-11-02 11:51:48 +0200

    Major changes, file now a stringmap

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,127 @@
-// package carltest;
+//package jono.testing;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
     private static int countLines = 0;
     private static String[] fileArray;
-    private static Object[] objArray;
-    private static Object[] collectedSettings;
+    private static boolean USE_ZONE = false;
 
     public static void main(String[] args) {
+        //file to parse
+        String file = "collect.settings";
+
         //Creates an object from a file, and prints it to console
-        deserializeFile("collect.settings", false);
-        collectedSettings = fileToObject("collect.settings");
-//        System.out.println(collectedSettings[0]);
-        Map<String, String> newMap = (Map) collectedSettings[0];
-//        System.out.println(newMap.values());
-        printMap(newMap);
+        deserializeFile(file, true);
+
+        Object[] collectedSettings = fileToObject(file);
+        System.out.println("Number of objects : " + collectedSettings.length);
+
+        Map<String, String> newMap;
+
+        // Create an array list of maps, mapsList
+        List<Map<String, String>> mapsList = new ArrayList<Map<String, String>>();      //Creates a list of maps that are found in the 'collectedSettings' object
+
+        //Runs through the array of objects  and adds to mapsList
+        for (int i = 0; i < collectedSettings.length; i++) {
+            //noinspection unchecked
+            newMap = (Map) collectedSettings[i];                                        //creates a map from the object
+            mapsList.add(i, newMap);                                                    //adds each map to the list
+        }
+
+
+        /*  TESTING :  OUTPUT  - can delete */
+        //        System.out.println("\n\nORIGINAL LIST:");
+        //        for (int i = 0; i < collectedSettings.length; i++) {
+        //            printMap(mapsList.get(i));
+        //            System.out.println("\n\n");
+        //        }
+
+        //update entry
+//        mapsList.get(0).put("username", "carl@test.com");
+        mapsList.get(0).put("username", "test");
+        mapsList.get(0).put("password", "t3stt3st");
+
+
+//        mapsList.get(0).put("autosend_wifi", true);
+//        mapsList.get(0).put("username", "carl@test.com");
+//        mapsList.get(0).put("username", "carl@test.com");
+//        mapsList.get(1).put("change_username", true);
+        System.out.println("\n\nUPDATED  LIST:");
+
+        for (int i = 0; i < collectedSettings.length; i++) {
+            printMap(mapsList.get(i));
+            System.out.println("\n\n");
+        }
+
+        serializeFile("collect_new.settings", mapsList);
+
+        //copies Alternate Data Stream
+        //this should probably be changed. TODO Use relative path instead of absolute path? Change this to your outfile path but keep the ":Zone.Identifier"
+        if (USE_ZONE){
+            try{
+                String s = getAlternateDataStream(file + ":Zone.Identifier").toString();
+                streamToFile(s, "C:\\Users\\Carl\\Workspace\\testEnv\\collect_new.settings:Zone.Identifier");
+            } catch(Exception ex){
+                System.out.println("EXCEPTION: " + ex);
+            }
+        }
+
+
+        //C:\Users\Carl\Workspace\testEnv
     }
 
-    private static void deserializeFile(String fileName, boolean debugOutput){
+    private static void deserializeFile(String fileName, boolean debugOutput) {
         try {
             FileInputStream fileIn = new FileInputStream(fileName);
             ObjectInputStream in = new ObjectInputStream(fileIn);
 
             countLines = 0;
+            Object obj = null;
+            Object[] objArray = new Object[2];
 
-            Object obj  = null;
-            objArray = new Object[2];
-
-            while( (obj = in.readObject()) != null ) {
-                if (debugOutput){
+            while ((obj = in.readObject()) != null) {
+                if (debugOutput) {
                     System.out.println(obj);
-
                 }
             }
-
             in.close();
             fileIn.close();
 
         } catch (IOException i) {
-//        	 System.out.println(i);
-        }
-
-        catch (ClassNotFoundException ex) {
+            //System.out.println(i);
+        } catch (ClassNotFoundException ex) {
             System.out.println(ex);
         }
-        if (debugOutput){
+        if (debugOutput) {
             System.out.println("");
         }
-//        System.out.println("\nThere are " + countLines + " line(s) in the file.");
+        //System.out.println("\nThere are " + countLines + " line(s) in the file.");
     }
 
-    private static void serializeFile(String fileName){
+    private static void serializeFile(String fileName, List<Map<String, String>> contents) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
+            //Run through the maps list
+            for (int i = 0; i < 2; i++) {
+                //                printMap(contents.get(i));
+                //                System.out.println("\n\n");
+                objectOutputStream.writeObject(contents.get(i));
+
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("oops...");
+            e.printStackTrace();
+        }
     }
 
-    private static Object[] fileToObject (String fileName){
-        Object [] tempArray = new Object[2];
+    private static Object[] fileToObject(String fileName) {
+        Object[] tempArray = new Object[2];
 
         try {
             //Initialize the file
@@ -67,18 +130,13 @@ public class Main {
 
             countLines = 0;
 
-            Object obj  = null;
+            Object obj = null;
 
-
-
-            while( (obj = in.readObject()) != null ) {
-
+            while ((obj = in.readObject()) != null) {
                 //Store the object in the array
                 tempArray[countLines] = obj;
                 countLines++;
             }
-
-
 
             in.close();
             fileIn.close();
@@ -86,22 +144,58 @@ public class Main {
             System.out.println("Array returned!");
 
         } catch (IOException i) {
-//        	 System.out.println(i);
-        }
-
-        catch (ClassNotFoundException ex) {
+            //        	 System.out.println(i);
+        } catch (ClassNotFoundException ex) {
             System.out.println(ex);
         }
-//        System.out.println("\nThere are " + countLines + " line(s) in the file.");
+        //        System.out.println("\nThere are " + countLines + " line(s) in the file.");
         return tempArray;
     }
 
     public static void printMap(Map mp) {
         Iterator it = mp.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
+            Map.Entry pair = (Map.Entry) it.next();
             System.out.println(pair.getKey() + " = " + pair.getValue());
-            it.remove(); // avoids a ConcurrentModificationException
+            // it.remove(); // avoids a ConcurrentModificationException
+        }
+    }
+
+    private static StringBuilder getAlternateDataStream(String filename) {
+        File ads = new File(filename);
+        FileInputStream inputStream = null;
+        StringBuilder sb = new StringBuilder("");
+        try {
+            inputStream = new FileInputStream(ads);
+            System.out.println("Data Steam size in bytes : " + inputStream.available());
+            int content;
+            while ((content = inputStream.read()) != -1) {
+                sb.append((char) content);
+                System.out.print((char) content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null)
+                    inputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return sb;
+    }
+
+    private static void streamToFile(String content, String filename) {
+        try {
+            OutputStream outputStream = new FileOutputStream(filename);
+            Writer writer = new OutputStreamWriter(outputStream);
+            writer.write(content);
+            writer.close();
+        } catch (FileNotFoundException f) {
+            f.printStackTrace();
+        } catch (IOException i) {
+            i.printStackTrace();
         }
     }
 }
